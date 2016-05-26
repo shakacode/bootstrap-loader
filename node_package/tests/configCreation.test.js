64ce260e133380e5e7065d9cffeabbc0d3a8939c
Andrew Ralston <andrew.ralston@novu.com>
2016-05-26 17:06:14 -0500

    test framework

--- node_package/tests/configCreation.test.js
+++ node_package/tests/configCreation.test.js
@@ -3,13 +3,10 @@ import proxyquire from 'proxyquire';
 import { userConfigFileExists, createConfig } from '../../src/bootstrap.config.js';
 
 test('userConfigFileExists returns true for files that exist', (assert) => {
-  var fileExists = function (userConfigPath) {
-    return true;
-  };
-  var bootstrapConfig = proxyquire('../../src/bootstrap.config.js', {
-   './fileExists': fileExists
- });
- var result = bootstrapConfig.userConfigFileExists('../somePath');
+  assert.plan(1);
+  console.log(__dirname + 'configCreation.test.js');
+  var result = userConfigFileExists(__dirname + '/configCreation.test.js');
 
- assert.equal(result, true);
+  assert.equal(result, true);
+  assert.end();
 });
