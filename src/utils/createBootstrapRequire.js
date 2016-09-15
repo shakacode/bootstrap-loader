5340fe836702dd58d36e6394d1788671e281d562
Andrey Pavlov <paffa4@gmail.com>
2016-09-15 12:15:32 +0300

    https://github.com/shakacode/bootstrap-loader/issues/172

--- src/utils/createBootstrapRequire.js
+++ src/utils/createBootstrapRequire.js
@@ -12,7 +12,7 @@ export default function(module, bootstrapVersion, bootstrapPath) {
   const scriptsPath = (
     parseInt(bootstrapVersion, 10) === 3 ?
     ['assets', 'javascripts', 'bootstrap'] :
-    ['js', 'dist']
+    ['js', 'src']
   );
   const bootstrapModule = path.join(bootstrapPath, ...scriptsPath, module);
   return `require (${JSON.stringify(bootstrapModule)});`;
