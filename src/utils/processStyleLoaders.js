0d99b0b9d0877302032f6c6b631301e12299cbbf
Judah Meek <judah.meek@gmail.com>
2016-09-18 04:06:25 +0000

    Make injection of resolve-url & sass sourcemap optional
    Addresses #52 and #53
    
    (cherry picked from commit 1a19f0e159be5edef46a677d87426b93380f0c49)

--- src/utils/processStyleLoaders.js
+++ src/utils/processStyleLoaders.js
@@ -8,7 +8,7 @@ import escapeRegExp from 'escape-regexp';
  * @param {string[]} loaders
  * @returns {string[]}
  */
-export default function(loaders) {
+export default function({ loaders, disableSassSourceMap, disableResolveUrlLoader }) {
   if (!Array.isArray(loaders)) {
     throw new Error(`
 Specify your loaders as an array.
@@ -16,49 +16,59 @@ Default is ['style', 'css', 'sass']
     `);
   }
 
+  if (disableSassSourceMap && disableResolveUrlLoader) {
+    return loaders;
+  }
+
   // We need to match user loaders in different formats:
   // 'sass', 'sass-loader', 'sass?someParam' etc.
   const getLoaderRegExp = module => (
     new RegExp(`^${escapeRegExp(module)}(?:-loader)?(?:\\?.*)?$`)
   );
-  const sassLoaderRegExp = getLoaderRegExp('sass');
-  const resolveUrlLoaderRegExp = getLoaderRegExp('resolve-url');
 
+
+  const sassLoaderRegExp = getLoaderRegExp('sass');
   const sassLoader = (
     loaders.find(loader => sassLoaderRegExp.test(loader))
   );
-  const resolveUrlLoader = (
-    loaders.find(loader => resolveUrlLoaderRegExp.test(loader))
-  );
-
-  if (!sassLoader) {
-    throw new Error(`
-      I can't find 'sass-loader'.
-      Add it to array of loaders in .bootstraprc.
-    `);
-  }
-
-  const sassLoaderQuery = sassLoader.split('?')[1];
-
-  // We need to check if user's loader already contains sourceMap param
-  // And if it's not there - inject it
-  let sassLoaderWithSourceMap;
-  if (sassLoaderQuery) {
-    sassLoaderWithSourceMap = (
-      sassLoaderQuery.includes('sourceMap') ?
-      sassLoader :
-      `${sassLoader}&sourceMap`
-    );
-  } else {
-    sassLoaderWithSourceMap = `${sassLoader}?sourceMap`;
-  }
-
   const sassLoaderIndex = loaders.indexOf(sassLoader);
 
-  loaders[sassLoaderIndex] = sassLoaderWithSourceMap;
+  if (!disableSassSourceMap) {
+    if (!sassLoader) {
+      throw new Error(`
+        I can't find 'sass-loader'.
+        Add it to array of loaders in .bootstraprc.
+      `);
+    }
 
-  if (!resolveUrlLoader) {
-    loaders.splice(sassLoaderIndex, 0, 'resolve-url');
+    const sassLoaderQuery = sassLoader.split('?')[1];
+
+    // We need to check if user's loader already contains sourceMap param
+    // And if it's not there - inject it
+    let sassLoaderWithSourceMap;
+    if (sassLoaderQuery) {
+      sassLoaderWithSourceMap = (
+        sassLoaderQuery.includes('sourceMap') ?
+        sassLoader :
+        `${sassLoader}&sourceMap`
+      );
+    } else {
+      sassLoaderWithSourceMap = `${sassLoader}?sourceMap`;
+    }
+
+
+    loaders[sassLoaderIndex] = sassLoaderWithSourceMap;
+  }
+
+  if (!disableResolveUrlLoader) {
+    const resolveUrlLoaderRegExp = getLoaderRegExp('resolve-url');
+    const resolveUrlLoader = (
+      loaders.find(loader => resolveUrlLoaderRegExp.test(loader))
+    );
+
+    if (!resolveUrlLoader) {
+      loaders.splice(sassLoaderIndex, 0, 'resolve-url');
+    }
   }
 
   return loaders;
