3d3cb6f20e90e8d86c88abd704402e1ac81aee16
Kevin Huang <kevin.ziwen.huang@gmail.com>
2016-11-16 00:46:31 -0800

    Bring examples/basic webpack configs up to compliance with -loaders suffix

--- examples/basic/webpack.prod.config.js
+++ examples/basic/webpack.prod.config.js
@@ -42,30 +42,30 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.css$/, loader: ExtractTextPlugin.extract({
-        fallbackLoader: 'style', loader: 'css!postcss',
+        fallbackLoader: 'style-loader', loader: 'css-loader!postcss-loader',
       }) },
       { test: /\.scss$/, loader: ExtractTextPlugin.extract({
-        fallbackLoader: 'style', loader: 'css!postcss!sass',
+        fallbackLoader: 'style-loader', loader: 'css-loader!postcss-loader!sass-loader',
       }) },
 
       {
         test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
         // Limiting the size of the woff fonts breaks font-awesome ONLY for the extract text plugin
         // loader: "url?limit=10000"
-        loader: 'url',
+        loader: 'url-loader',
       },
       {
         test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
-        loader: 'file',
+        loader: 'file-loader',
       },
 
       // Use one of these to serve jQuery for Bootstrap scripts:
 
       // Bootstrap 4
-      { test: /bootstrap\/dist\/js\/umd\//, loader: 'imports?jQuery=jquery' },
+      { test: /bootstrap\/dist\/js\/umd\//, loader: 'imports-loader?jQuery=jquery' },
 
       // Bootstrap 3
-      { test: /bootstrap-sass\/assets\/javascripts\//, loader: 'imports?jQuery=jquery' },
+      { test: /bootstrap-sass\/assets\/javascripts\//, loader: 'imports-loader?jQuery=jquery' },
     ],
   },
 
